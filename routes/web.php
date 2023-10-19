2e5224c0414a0238439cc3d54a9e596989f084fb
HartLianger <blyueeyes@yahoo.com>
2023-10-19 21:00:41 +0700

    awal

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
 
-Route::get('/',            [MahasiswaController::class,'index']);
-Route::get('/proses-form', [MahasiswaController::class,'prosesForm']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
